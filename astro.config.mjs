3d8d94f51bcea569e09d8e146c30d449d5356278
magurokanno <dearbear.kuma@gmail.com>
2024-07-26 00:08:27 +0900

    設定変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,19 @@ import remarkHeadingId from "remark-heading-id";
 
 // https://astro.build/config
 export default defineConfig({
+	// ***** トップレベル
+	// EDIT 最終的な出力URL
+	site: "https://www.my-site.dev",
+
+	// ****- vite
+	vite: {},
+
+	// ***** ビルド
+	// ****- JSやCSSなどのアセットが出力されるディレクトリ
+	build: {
+		assets: "_assets",
+	},
+
 	// *****MD
 	// ****-MDの見出しIDを設定する：remark-heading-id
 	markdown: {
