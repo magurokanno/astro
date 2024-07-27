5023999c166010d3d9a223c04218a0447d7687fe
magurokanno <dearbear.kuma@gmail.com>
2024-07-28 00:36:36 +0900

    @astrojs/sitemapを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from "astro/config";
 import remarkHeadingId from "remark-heading-id";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
 	// ***** トップレベル
 	// EDIT 最終的な出力URL
 	site: "https://www.my-site.dev",
-
+	// ****- 拡張機能：サイトマップ生成
+	integrations: [sitemap()],
 	// ****- vite
 	vite: {},
-
 	// ***** ビルド
 	// ****- JSやCSSなどのアセットが出力されるディレクトリ
 	build: {
 		assets: "_assets",
 	},
-
 	// *****MD
 	// ****-MDの見出しIDを設定する：remark-heading-id
 	markdown: {
