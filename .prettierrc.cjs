477f2a23df5c7193e5bb63e3dd0e235c23b9466f
magurokanno <dearbear.kuma@gmail.com>
2024-07-31 01:04:20 +0900

    リンター、フォーマッターにルール記述

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,26 @@
 module.exports = {
 	plugins: [require.resolve("prettier-plugin-astro")],
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: false,
+	semi: true,
+	singleQuote: true,
+	quoteProps: "as-needed",
+	trailingComma: "es5",
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: "always",
+	jsxBracketSameLine: false,
+	jsxSingleQuote: false,
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: "preserve",
+	htmlWhitespaceSensitivity: "strict",
+	vueIndentScriptAndStyle: true,
+	endOfLine: "lf",
+	embeddedLanguageFormatting: "auto",
+	singleAttributePerLine: false,
+
 	overrides: [
 		{
 			files: "*.astro",
